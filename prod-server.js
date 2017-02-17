bfd80bc66807938e80cd342c8e5b5a1e321cbf3b
Robert <robertsi>
2017-02-17 05:08:28 +0100

    added jsbeauify

--- prod-server.js
+++ prod-server.js
@@ -4,11 +4,11 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, "/dist")));
 
-app.get("*", function (req, res) {
+app.get("*", function(req, res) {
 	res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
-app.listen(3000, function (err) {
+app.listen(3000, function(err) {
 	if (err) {
 		console.log(err);
 	}

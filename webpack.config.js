2c0876e9f4f5fd78d66a05032985a811d40effb0
Robert <robertsi>
2017-02-18 18:01:04 +0100

    improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
-const copyWebpackPlugin = require("copy-webpack-plugin");
+//const copyWebpackPlugin = require("copy-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 const DEVELOPMENT = process.env.NODE_ENV === "development";
@@ -17,16 +17,15 @@ const plugins = DEVELOPMENT ? [
 	// enable HMR globally
 	new webpack.NamedModulesPlugin(),
 	// prints more readable module names in the browser console on HMR updates,
-	new copyWebpackPlugin([{
-		from: "index.html",
-		to: "index.html"
-	}])
+	new HTMLWebpackPlugin({
+		template: "index-template-dev.html"
+	})
 ] : [
 	new webpack.optimize.UglifyJsPlugin({
 		sourceMap: true
 	}),
 	new HTMLWebpackPlugin({
-		template: "index-template.html"
+		template: "index-template-prod.html"
 	})
 ];
 
